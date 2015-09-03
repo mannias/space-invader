34b89189e4e03ec57b337776b190f0522bba9f49
Matias Domingues <mannias@gmail.com>
2015-09-03 17:20:06 -0300

    Initial commit

---
+++ Assets/Scripts/LaserControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserControl : MonoBehaviour {
+
+	private bool isShowingLaser = false;
+	private Renderer renderer;
+
+	void Start() {
+		this.renderer = this.gameObject.GetComponent<Renderer> ();
+	}
+
+
+	void showLaser()
+	{
+		if( isShowingLaser ) return;
+		isShowingLaser = true;
+		this.renderer.enabled = true;
+		
+		yield return new WaitForSeconds(.05);
+		resetLaser();
+		isShowingLaser = false;
+	}
+
+	void resetLaser()
+	{    
+		this.renderer.enabled = false;
+	}
+
+}
