8262a9e84d59bd44cbc13606cd98452521667322
Teresa di Tada <tditada@gmail.com>
2015-09-13 20:44:53 -0300

    Proper gitignore, moving with addForce

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -5,26 +5,38 @@ public class ShipController : MonoBehaviour {
 
 	private Vector3 position;
 	private LasersController lasers;
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		GameObject laser = GameObject.Find ("Lasers"); 
 		this.lasers = laser.GetComponent<LasersController>(); 
 		this.position = this.transform.position;
+		rb = this.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.W)) {
-			position.z += 1;
-			this.transform.position = position;
-		} else if (Input.GetKey (KeyCode.S)) {
-			position.z -= 1;
-			this.transform.position = position;
+
+	}
+
+	void FixedUpdate(){
+		if (Input.GetAxis ("Horizontal") > 0) {
+			rb.AddForce (transform.right * 50);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			rb.AddForce (transform.right * -50);
+		} else if (Input.GetAxis ("Vertical") > 0) {
+			rb.AddForce (transform.up * 50);	
+		} else if (Input.GetAxis ("Vertical") < 0) {
+			rb.AddForce (transform.up * -50);	
+		} else {
+			rb.velocity=new Vector3(0,0,0);
 		}
 		if (Input.GetKey (KeyCode.Space)) {
 			this.lasers.shotLasers();
 		}
-
 	}
+
+
+
 }
