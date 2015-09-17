6c8739bfdb4498cc0a7c34b550f8df9e46df302d
Facundo Martinez <fnmartinez88@gmail.com>
2015-09-16 21:11:35 -0300

    Se termina el MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Canvas QuitMenu;
+	public Button StartBtn;
+	public Button ExitBtn;
+
+	// Use this for initialization
+	void Start() {
+		//QuitMenu = QuitMenu.GetComponent<Canvas> ();
+		//StartBtn = StartBtn.GetComponent<Button> ();
+		//ExitBtn = ExitBtn.GetComponent<Button> ();
+		RestoreMainMenu();
+	}
+
+	public void OnExitPress() {
+		QuitMenu.enabled = true;
+		StartBtn.enabled = false;
+		ExitBtn.enabled = false;
+	}
+
+	public void OnExitNoPress() {
+		RestoreMainMenu ();
+	}
+
+	public void OnExitYesPress() {
+		Application.Quit ();
+	}
+
+	public void RestoreMainMenu() {
+		QuitMenu.enabled = false;
+		StartBtn.enabled = true;
+		ExitBtn.enabled = true;
+	}
+
+	public void OnStartPress() {
+		Application.LoadLevel (1);
+	}
+}
