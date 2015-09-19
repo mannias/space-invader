cf8b7336c58ebc5b5f3c15b091373fce8f35568b
Teresa di Tada <tditada@gmail.com>
2015-09-18 23:52:46 -0300

    GameController, lifes, pause button, score

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum TypeOfEnemy {Asteroid, SimpleSpaceShip}; 
+
+public class GameController : MonoBehaviorSingleton<GameController> {
+
+	int score;
+	public Text scoreText;
+
+	void Start () {
+		score = 0;
+		UpdateScore ();
+	}
+
+	public void AddScore(TypeOfEnemy enemy){
+		if (enemy == TypeOfEnemy.Asteroid) {
+			score += 10;
+		} else if (enemy == TypeOfEnemy.SimpleSpaceShip) {
+			score+=30;
+		}
+		UpdateScore ();
+	}
+	
+	void UpdateScore () {
+		scoreText.text = "Score: " + score;
+
+	}
+
+	IEnumerator Countdown()
+	{
+		//centralText.text = "3";
+		yield return new WaitForSeconds(1);
+		//centralText.text = "2";
+		yield return new WaitForSeconds(1);
+		//centralText.text = "1";
+		yield return new WaitForSeconds(1);
+		//centralText.text = "Go!!!";
+		yield return new WaitForSeconds(1);
+		//centralText.text = "";
+	}
+}
